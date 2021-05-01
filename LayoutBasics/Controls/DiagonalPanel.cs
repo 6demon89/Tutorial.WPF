750845e1f1f0327c24e9bca3031ba0a201981650
6demon89 <61165147+6demon89@users.noreply.github.com>
2021-05-01 11:57:14 +0200

    add Basic Layout Project

---
+++ LayoutBasics/Controls/DiagonalPanel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LayoutBasics.Controls
+{
+    //Class Hierarchy WPF
+    //Object
+    //DispatcherObject
+    //DependencyObject
+    //Visual
+    //UIElement
+    //FrameworkElement (Width,Hight,Margin,etc)
+    //Panel Class (UI Collection)
+
+    //class Hierarchy WinUI2
+    //Object
+    //DependencyObject
+    //UIElement
+    //FrameworkElement (Width,Hight,Margin,etc)
+    //Panel Class (UI Collection)
+
+    //Layout Process : is based on 3 steps
+    //1.Measure => calculation of size
+    //2.Arrange => arranging position
+    //3.Rendering 
+    public class DiagonalPanel : Panel
+    {
+        /// <summary>
+        /// Calculate the size, as well as size of the children
+        /// </summary>
+        /// <param name="availableSize"> Size of parent.</param>
+        /// <returns>new desired size</returns>
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            var newSize = new Size();
+
+            //InternalChildren panel collection
+            foreach (UIElement item in InternalChildren)
+            {
+                //child can not be bigger then parent
+                item.Measure(availableSize);
+                newSize.Width += item.DesiredSize.Width;
+                newSize.Height += item.DesiredSize.Height;
+            }
+
+            return newSize;
+        }
+
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            var location = new Point();
+            foreach (UIElement item in InternalChildren)
+            {
+                item.Arrange(new Rect(location,item.DesiredSize));
+                location.X += item.DesiredSize.Width;
+                location.Y += item.DesiredSize.Height;
+            }
+            return finalSize;
+        }
+    }
+}
---
+++ LayoutBasics/Controls/SimpleCanvas.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LayoutBasics.Controls
+{
+    public class SimpleCanvas : Panel
+    {
+
+
+        public static double GetTop(DependencyObject obj)
+        {
+            return (double)obj.GetValue(TopProperty);
+        }
+
+        public static void SetTop(DependencyObject obj, double value)
+        {
+            obj.SetValue(TopProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for Top.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TopProperty =
+            DependencyProperty.RegisterAttached("Top", 
+                typeof(double), 
+                typeof(SimpleCanvas), 
+                new PropertyMetadata(0.0,TopPropertyChanged)
+                );
+
+        private static void TopPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var child = d as FrameworkElement;
+            if(child != null)
+            {
+                var canvas = child.Parent as SimpleCanvas;
+                if(canvas != null)
+                {
+                    canvas.InvalidateArrange();
+                    canvas.UpdateLayout();
+                }
+            }
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            var location = new Point();
+            foreach (UIElement item in InternalChildren)
+            {
+                location.Y = GetTop(item);
+                item.Arrange(new Rect(location, item.DesiredSize));
+            }
+
+            return finalSize;
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            foreach (UIElement item in InternalChildren)
+                item.Measure(availableSize);
+            return base.MeasureOverride(availableSize);
+        }
+    }
+}
